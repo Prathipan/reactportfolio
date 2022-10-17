662ef465186346a2469555157f1bce043e74f279
Prathipan <prathipan24p@gmail.com>
2022-10-17 10:28:00 +0530

    added cart poject

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -4,6 +4,7 @@ import "./projects.css";
 import tictactoe from '../../assets/tictactoe.png'
 import todo from '../../assets/todoApp.png'
 import moviesApp from '../../assets/moviesApp.png'
+import shoppingCart from '../../assets/shoppingCart.png'
 
 // todo = https://flourishing-strudel-9a7e18.netlify.app/
 //tic tac toe https://vocal-flan-61c2f4.netlify.app/
@@ -17,6 +18,12 @@ const Projects = () => {
       img :  tictactoe
     },
     {
+      title : "Shopping cart",
+      description : "It is a simple shopping cart page , which is having set of products. we can add to cart and remove from cart. Total can be calculated based on cart added and removed",
+      url : "https://prathipan-shopping-cart.netlify.app",
+      img : shoppingCart
+    },
+    {
       title : "ToDo App",
       description : "Performed CRUD operations . I have used fetch API to perform get , put, post, delete operations. I have used basic hooks like 'useState'&'useEffect",
       url : "https://flourishing-strudel-9a7e18.netlify.app/",
@@ -28,6 +35,7 @@ const Projects = () => {
       url : "https://cosmic-croquembouche-c40e58.netlify.app/",
       img : moviesApp
     }
+    
   ]
   return (
     <>
